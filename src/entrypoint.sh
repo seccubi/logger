b64bdb6c72629ec2d3bc8b92fd099c5cb90597d6
Andriy Kutsynyak <akutsynyak@gmail.com>
2021-04-10 23:26:22 -0500

    entrypoint restart on fail

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -3,7 +3,7 @@ cd /fluent-bit/etc
 
 curl -X GET $REACT_APP_API_ENTRYPOINT/api/v1/assets/certificates/$1 --output config.zip
 
-unzip -o config.zip
+unzip -o config.zip || exit 1
 rm config.zip
 
 /fluent-bit/bin/fluent-bit -c ./fluent-bit.conf
