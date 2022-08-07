0a25f6a7ddf5ea891f88841dc439ff67b2e54850
Andriy Kutsynyak <akutsynyak@gmail.com>
2022-08-07 13:38:35 -0500

    Fix the bug with non refreshed asset status

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -38,7 +38,7 @@ downloadConfig $1
 iteration=0
 while :
 do
-  ps -aux | grep /fluent-bit/bin/fluent-bit | grep -v grep || /fluent-bit/bin/fluent-bit -vvv -c /fluent-bit/etc/fluent-bit.conf
+  ps -aux | grep /fluent-bit/bin/fluent-bit | grep -v grep || /fluent-bit/bin/fluent-bit -vvv -c /fluent-bit/etc/fluent-bit.conf &
   iteration=$((iteration+1))
   if [ $iteration -gt 10 ]
     then
@@ -50,14 +50,14 @@ do
         then
           echo "$(date) Restarting due to config updated\n"
           downloadConfig $1
-          killall fluent-bit && /fluent-bit/bin/fluent-bit -vvv -c /fluent-bit/etc/fluent-bit.conf
+          killall fluent-bit && /fluent-bit/bin/fluent-bit -vvv -c /fluent-bit/etc/fluent-bit.conf &
           latestSync=$i
         fi
         if [ $j = 2 ] && [ ${i:0:1} = "0" ]
         then
           echo "$(date) Restarting due to broken connection\n"
           downloadConfig $1
-          killall fluent-bit && /fluent-bit/bin/fluent-bit -vvv -c /fluent-bit/etc/fluent-bit.conf
+          killall fluent-bit && /fluent-bit/bin/fluent-bit -vvv -c /fluent-bit/etc/fluent-bit.conf &
           latestSync=$i
         fi
         j=$((j+1))
